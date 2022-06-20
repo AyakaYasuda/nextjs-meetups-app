af89da1f4c47d8cd90410b0899e85880813cc1ae
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-06-19 22:14:40 -0700

    added getStaticPaths func to meetup detail page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,28 @@ const meetupDetails = () => {
   );
 };
 
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [{ params: { meetupId: 'm1' } }, { params: { meetupId: 'm2' } }],
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        title: 'A first meetup',
+        address: 'Some address 1, 12345 Some city',
+        description: 'The a first meetup',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Rathaus_and_Marienplatz_from_Peterskirche_-_August_2006.jpg/1200px-Rathaus_and_Marienplatz_from_Peterskirche_-_August_2006.jpg',
+      },
+    },
+  };
+};
+
 export default meetupDetails;
