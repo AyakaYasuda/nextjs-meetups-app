b81daabaf4496ed94d984eb049bfd2b01ebdffda
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-06-19 21:56:44 -0700

    implemented SSG and SSR fetching data at HomePage

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,40 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-const HomePage = () => {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+const HomePage = (props) => {
+  return <MeetupList meetups={props.meetups} />;
+};
+
+/*
+SSR (Static Side Rendering) - running whenever the data is updated after building has been done
+
+export const getServerSideProps = async (context) => {
+  const req = context.req;
+  const res = context.res;
+
+  // fetch data from an API or a database
+
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
+};
+*/
+
+/*
+SSG (Static Site Generation) - running during the building process
+*/
+
+export const getStaticProps = async () => {
+  // fetch data from an API or a database
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 10,
+    // re-generate the page on the server every 10 secs, meaning there's no need to build it every time data is updated
+  };
 };
 
 export default HomePage;
