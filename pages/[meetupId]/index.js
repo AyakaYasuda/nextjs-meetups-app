268b8f06ce788bb3439e047f53db349649aa5786
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-06-20 19:10:58 -0700

    connected the app to mongodb & added metadata

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,35 +1,70 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
+import { MongoClient, ObjectId } from 'mongodb';
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-const meetupDetails = () => {
+const meetupDetails = (props) => {
   return (
-    <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Rathaus_and_Marienplatz_from_Peterskirche_-_August_2006.jpg/1200px-Rathaus_and_Marienplatz_from_Peterskirche_-_August_2006.jpg"
-      title="A first meetup"
-      address="Some address 1, 12345 Some city"
-      description="The a first meetup"
-    />
+    <Fragment>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name="description" content={props.meetupData.description} />
+      </Head>
+      <MeetupDetail
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+      />
+    </Fragment>
   );
 };
 
 export const getStaticPaths = async () => {
+  const client = await MongoClient.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mycluster.xq6fs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [{ params: { meetupId: 'm1' } }, { params: { meetupId: 'm2' } }],
+    paths: meetups.map((meetup) => ({
+      params: {
+        meetupId: meetup._id.toString(),
+      },
+    })),
   };
 };
 
 export const getStaticProps = async (context) => {
   const meetupId = context.params.meetupId;
 
+  const client = await MongoClient.connect(
+    'mongodb+srv://ayakayasuda:Lemichan818@mycluster.xq6fs.mongodb.net/meetups?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+
+  console.log(selectedMeetup);
+
+  client.close();
+
   return {
     props: {
       meetupData: {
-        id: meetupId,
-        title: 'A first meetup',
-        address: 'Some address 1, 12345 Some city',
-        description: 'The a first meetup',
-        image:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Rathaus_and_Marienplatz_from_Peterskirche_-_August_2006.jpg/1200px-Rathaus_and_Marienplatz_from_Peterskirche_-_August_2006.jpg',
+        image: selectedMeetup.image,
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
       },
     },
   };
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,21 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
+import { MongoClient } from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A first meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Rathaus_and_Marienplatz_from_Peterskirche_-_August_2006.jpg/1200px-Rathaus_and_Marienplatz_from_Peterskirche_-_August_2006.jpg',
-    address: 'Some address 1, 12345 Some city',
-    description: 'This is a first meetup',
-  },
-  {
-    id: 'm2',
-    title: 'A second meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Rathaus_and_Marienplatz_from_Peterskirche_-_August_2006.jpg/1200px-Rathaus_and_Marienplatz_from_Peterskirche_-_August_2006.jpg',
-    address: 'Some address 2, 12345 Some city',
-    description: 'This is a second meetup',
-  },
-];
-
 const HomePage = (props) => {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
+  );
 };
 
 /*
@@ -46,9 +41,25 @@ SSG (Static Site Generation) - running during the building process
 
 export const getStaticProps = async () => {
   // fetch data from an API or a database
+  const client = await MongoClient.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mycluster.xq6fs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  console.log(process.env.DEVELOPMENT_ENV_VARIABLE);
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 10,
     // re-generate the page on the server every 10 secs, meaning there's no need to build it every time data is updated
